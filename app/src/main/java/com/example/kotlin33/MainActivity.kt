2dcceb7f666f073dcb072a6191b26f420d63924e
Antua <suleykin2005@mail.ru>
2025-12-12 01:16:03 +0300

    add structure (no functional)

--- app/src/main/java/com/example/kotlin33/MainActivity.kt
+++ app/src/main/java/com/example/kotlin33/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin33.ui.theme.Kotlin33Theme
+import com.example.kotlin33.presentation.theme.Kotlin33Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
